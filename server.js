6275d256dbe69e9e0f0fa98d6a4dc0c3ac49b351
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-11-27 22:35:45 -0700

    rearrange files to function together

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 
 var app =  express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
